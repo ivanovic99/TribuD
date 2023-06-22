ec8f154b37bf2b3e24179c316e6a7e3af79be449
ivan <ivanerlich@gmail.com>
2023-06-22 18:35:00 -0300

    index.js modified so it can use Render servicies now, we are testing if it actually works (second attempt)

--- backend/index.js
+++ backend/index.js
@@ -4,19 +4,6 @@ const mongoose = require('mongoose');
 const routes = require('./app/routes');
 
 async function startServer() {
-  // Conexión a la base de datos en Render
-  
-  const dbUrl = 'https://api.render.com/deploy/srv-ciabg3d9aq007tfuphj0?key=PyypVQZLRV8';
-  await mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'Error al conectar a la base de datos:'));
-  db.once('open', () => {
-    console.log('Conexión exitosa a la base de datos');
-  });
-
   const app = express();
 
   // Middleware
@@ -25,6 +12,24 @@ async function startServer() {
 
   // Rutas
   app.use('/api', routes);
+
+  // Conexión a la base de datos en Render
+  const dbUrl = process.env.DATABASE_URL;
+  if (!dbUrl) {
+    console.error('La variable de entorno DATABASE_URL no está configurada.');
+    return;
+  }
+
+  try {
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conexión exitosa a la base de datos');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+    return;
+  }
 
   // Iniciar el servidor
   const port = process.env.PORT || 8080;
