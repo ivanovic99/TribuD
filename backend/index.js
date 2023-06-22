e6763ba1430fc2170ed98fe74012753327b3ee97
ivan <ivanerlich@gmail.com>
2023-06-22 18:22:54 -0300

    index.js modified so it can use Render servicies now, we are testing if it actually works (first attempt)

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-const connectToDatabase = require('./config/db');
+const mongoose = require('mongoose');
 const routes = require('./app/routes');
 
 async function startServer() {
-  // Conexión a la base de datos
-  const db = await connectToDatabase();
+  // Conexión a la base de datos en Render
+  
+  const dbUrl = 'https://api.render.com/deploy/srv-ciabg3d9aq007tfuphj0?key=PyypVQZLRV8';
+  await mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'Error al conectar a la base de datos:'));
+  db.once('open', () => {
+    console.log('Conexión exitosa a la base de datos');
+  });
 
   const app = express();
 
@@ -13,13 +23,13 @@ async function startServer() {
   app.use(express.json());
   app.use(cors());
 
-
   // Rutas
   app.use('/api', routes);
 
   // Iniciar el servidor
-  app.listen(8080, () => {
-    console.log('Servidor iniciado en el puerto 8080');
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Servidor iniciado en el puerto ${port}`);
   });
 }
 
