2dacd2d84dce8e09207e17387f5d557ec277cb04
ivan <ivanerlich@gmail.com>
2023-06-26 20:27:57 -0300

    ticket data attributes used

--- backend/app/routes/ticketRoutes.js
+++ backend/app/routes/ticketRoutes.js
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
   try {
     const productId = req.params.productId;
     const { title, description, status, priority, severity, task, client } = req.body;
-    const ticketData = { title, description, status };
+    const ticketData = { title, description, status,priority, severity, task, client };
     const createdTicket = await ticketController.createTicketForProduct(productId, ticketData);
     res.status(201).json(createdTicket);
   } catch (error) {
